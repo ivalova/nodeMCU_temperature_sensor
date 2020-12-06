84409dd5ef5174bc523fac1945e589b21253a4e4
ivalova <39760740+ivalova@users.noreply.github.com>
2020-12-06 08:49:55 +0100

    Replace DS18B20 with BMP280

--- nodeMCU_temperature_sensor.ino
+++ nodeMCU_temperature_sensor.ino
@@ -3,13 +3,14 @@
 #include "analogRead.h"
 
 #include <ESP8266WiFi.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 
-unsigned long timer_period_s{120};
-const uint8_t temperature_sensor_pin{D7};
+const uint8_t sensor_address{0x76}; //bmp280 address
+const unsigned long timer_period_s{120};
+
 
 formFiller form_filler(google_form_id,
                        "entry.20675324",
@@ -17,8 +18,7 @@ formFiller form_filler(google_form_id,
                        "entry.1563906843",
                        "entry.1884689726");
 
-OneWire one_wire(temperature_sensor_pin);
-DallasTemperature temperature_sensor(&one_wire);
+Adafruit_BMP280 environment_sensor;
 WiFiUDP ntp_udp;
 NTPClient time_client(ntp_udp);
 
@@ -37,23 +37,26 @@ void loop() {
   //Check WiFi connection status
   if (WiFi.status() == WL_CONNECTED) {
 
-    temperature_sensor.begin();
-    uint8_t device_count = temperature_sensor.getDS18Count();
-    if (1 == device_count)
+    bool is_begin_successful = environment_sensor.begin(sensor_address);
+    if (true == is_begin_successful)
     {
       float voltage = getSupplyVoltage();
       Serial.print("Voltage: ");
       Serial.println(voltage);
 
-      temperature_sensor.requestTemperatures();
-      float temperature = temperature_sensor.getTempCByIndex(0);
+      float temperature = environment_sensor.readTemperature();
       Serial.print("Temperature: ");
       Serial.println(temperature);
-      form_filler.sendData(String(voltage),String(temperature));
+
+      float pressure = environment_sensor.readPressure();
+      Serial.print("Pressure: ");
+      Serial.println(pressure);
+
+      form_filler.sendData(String(voltage), String(temperature), String(), String(pressure));
     }
     else
     {
-      Serial.println("No 1DS8B20 sensor detected.");
+      Serial.println("Could not find a valid BMP280 sensor.");
     }
   }
   else {
@@ -70,7 +73,7 @@ void loop() {
   Serial.print(sleep_time_s);
   Serial.println(" seconds...");
 
-  ESP.deepSleep(sleep_time_s * 1e6); //deepSleep receives us
+  ESP.deepSleep(timer_period_s * 1e6); //deepSleep receives us
 }
 
 void connectToWiFi(void)
